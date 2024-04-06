1ff4530f4de360094e0823a2e4aa78d1d5bd3958
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-07 00:33:14 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDom from 'react-dom/client';
 import App from './App';
 
+
 const el = document.getElementById('root');
 const root = ReactDom.createRoot(el);
 
