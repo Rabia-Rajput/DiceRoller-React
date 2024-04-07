f9f477f3c120eeb0ccc2623ce17c90462708af8b
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-08 00:25:38 +0200

    updated file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+
 import DiceImage1 from './images/Dice1.png';
 import DiceImage2 from './images/Dice2.png';
 import DiceImage3 from './images/Dice3.png';
@@ -8,88 +9,83 @@ import DiceImage4 from './images/Dice4.png';
 import DiceImage5 from './images/Dice5.png';
 import DiceImage6 from './images/Dice6.png';
 
-
 function App() {
- const diceImages =[
+  const diceImages = [
     DiceImage1,
     DiceImage2,
     DiceImage3,
     DiceImage4,
     DiceImage5,
     DiceImage6
-  ]
-  const [image1, setImage1]=useState(diceImages[0]);
-  const [image2, setImage2]=useState(diceImages[1]);
-  const [sum, setSum]=useState(0);
-  const [condition, setCondition]=useState('');
-  const [winner, setWinner]= useState('');
-  const rollDice =()=>{
-    const dice1Image=Math.floor(Math.random()*6 );
-    const dice2Image=Math.floor(Math.random()*6 );
-    const newSum =(dice1Image + dice2Image +2);
+  ];
+  const [image1, setImage1] = useState(diceImages[0]);
+  const [image2, setImage2] = useState(diceImages[1]);
+  const [sum, setSum] = useState(0);
+  const [condition, setCondition] = useState('');
+  const [winner, setWinner] = useState('');
+  const [showError, setShowError] = useState(false);
+
+  const rollDice = () => {
+    if (!condition) {
+      setShowError(true);
+      return;
+    }
+
+    setShowError(false);
+
+    const dice1Image = Math.floor(Math.random() * 6);
+    const dice2Image = Math.floor(Math.random() * 6);
+    const newSum = dice1Image + dice2Image + 2;
     setSum(newSum);
     setImage1(diceImages[dice1Image]);
     setImage2(diceImages[dice2Image]);
-  
-    if(newSum===7 && condition=='equal'){
+
+    if (newSum === 7 && condition === 'equal') {
       setWinner('You won');
-    } else if(newSum > 7 && condition=='greaterthan'){
+    } else if (newSum > 7 && condition === 'greaterthan') {
       setWinner('You won');
-    }
-   else if(newSum < 7 && condition=='lessthan'){
+    } else if (newSum < 7 && condition === 'lessthan') {
       setWinner('You won');
-    }
-    else{
+    } else {
       setWinner('You lose');
     }
   };
-  const onConditionChange =(event)=>{
+
+  const onConditionChange = (event) => {
     setCondition(event.target.value);
-  }
-  
-
-
+    setShowError(false);
+  };
 
   return (
     <div className="App">
       <center>
-        <h1>
-          Welcome To Dice Roller Game
-        </h1>
+        <h1>Welcome To Dice Roller Game</h1>
         <div className='container'>
-          <img className='square' src={image1}/>
-          <div style={{display:'inline-block',width:'5px' }}  ></div>
-          <img className='square' src={image2} />
-          <div className='condition'> 
-            <h1>Select Condition</h1>
-            <div className='inputs'> 
-          <label  >
-            <input type="radio" checked={condition==='equal'} value={'equal'} onChange={onConditionChange} />
-            Sum will be 7
-          </label>
-          <label >
-            <input type="radio" checked={condition==='greaterthan'} value={'greaterthan'} onChange={onConditionChange}  />
-            Sum will be greater than  7
-          </label>
-          <label >
-            <input type="radio" checked={condition==='lessthan'} value={'lessthan'} onChange={onConditionChange}  />
-            Sum will be less than  7
-          </label>
+          <img className='square' src={image1} alt="Dice 1" />
+          <div style={{ display: 'inline-block', width: '5px' }}></div>
+          <img className='square' src={image2} alt="Dice 2" />
+          <div className='condition'>
+            <h1>Please select a condition before rolling the dice</h1>
+            <div className='inputs'>
+              <label>
+                <input type="radio" checked={condition === 'equal'} value={'equal'} onChange={onConditionChange} />
+                Sum will be 7
+              </label>
+              <label>
+                <input type="radio" checked={condition === 'greaterthan'} value={'greaterthan'} onChange={onConditionChange} />
+                Sum will be greater than 7
+              </label>
+              <label>
+                <input type="radio" checked={condition === 'lessthan'} value={'lessthan'} onChange={onConditionChange} />
+                Sum will be less than 7
+              </label>
+            </div>
           </div>
-          </div>
-         
-          
-
         </div>
-        <button className='btn' disabled={!condition} onClick={rollDice} type="button" class="btn btn-outline-primary">Roll Dice</button>
-        { <p>Sum : {sum}</p>
-                  }
-                  {
-                    <h1>{winner}  </h1> 
-                  }
-       
+        <button  disabled={!condition} onClick={rollDice} type="button" className="btn btn-outline-primary">Roll Dice</button>
+        <p className='sum'>Sum: {sum}</p>
+        <h1>{winner}</h1>
       </center>
-      
     </div>
   );
 }
